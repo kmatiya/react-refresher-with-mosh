c787605a870a031b2d1b257d19a24b07d921bb23
kmatiya <khormatiya>
2021-01-01 23:39:49 +0200

    Bind events to handler via constructor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,13 @@ class Counter extends Component {
     count: 0,
   };
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
   handleIncrement() {
-    console.log("Increment Clicked");
+    console.log("Increment Clicked",this);
   }
 
   render() {
