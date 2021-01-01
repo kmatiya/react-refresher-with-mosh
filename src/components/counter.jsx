9cbda2cb4de4a9207535bc3aa002d08b993f2f76
kmatiya <khormatiya>
2021-01-01 23:49:22 +0200

    Update state change

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,9 +5,9 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement =() => {
-    console.log("Increment Clicked",this);
-  }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
